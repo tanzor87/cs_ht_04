c96d863522b9204c2f8f6fb6f70426fd12579cef
Andrey <tanzor87@mail.ru>
2023-10-28 00:05:05 +0700

    add task 2 and 3

---
+++ task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string message = "ampty")
+{
+    if (message != "ampty")
+    {
+        Console.WriteLine(message);
+    }
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+
+}
+
+void SumValueNumber(int number)
+{    
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+
+int num = ReadNumber("Введите целое число: ");
+SumValueNumber(num);
---
+++ task_03/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int ReadNumber(string message = "ampty")
+{
+    if (message != "ampty")
+    {
+        Console.WriteLine(message);
+    }
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] ArrayAlements(int len)
+{
+    Console.WriteLine("Введите числа массива: ");
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = ReadNumber();
+    }
+    return array;
+}
+
+int lenArray = ReadNumber("Введите количество элементов массива: ");
+int[] array = ArrayAlements(lenArray);
+PrintArray(array);
